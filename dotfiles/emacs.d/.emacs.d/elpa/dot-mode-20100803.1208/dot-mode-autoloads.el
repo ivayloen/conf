a31fe5d292fdddb9a48f641486d917ce72f43744
Ivaylo Enchev <ivayloen@gmail.com>
2013-09-19 17:38:23 +0300

    update .emacs

---
+++ dotfiles/emacs.d/.emacs.d/elpa/dot-mode-20100803.1208/dot-mode-autoloads.el
@@ -0,0 +1,18 @@
+;;; dot-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("dot-mode-pkg.el" "dot-mode.el") (21038
+;;;;;;  4569 465297 167000))
+
+;;;***
+
+(provide 'dot-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; dot-mode-autoloads.el ends here
---
+++ dotfiles/emacs.d/.emacs.d/elpa/evil-nerd-commenter-20130909.2300/evil-nerd-commenter-pkg.el
@@ -0,0 +1,2 @@
+(define-package "evil-nerd-commenter" "20130909.2300" "Comment/uncomment lines efficiently. Like Nerd Commenter in Vim [github]" 'nil)
+
