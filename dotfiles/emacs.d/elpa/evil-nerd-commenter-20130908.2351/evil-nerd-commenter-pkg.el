701a19e406a6d3e9c35fa329691211b6fee3eb39
ienchev <ienchev@ie-ultrabook.(none)>
2014-03-14 12:24:47 +0100

    clean up

--- dotfiles/emacs.d/elpa/evil-nerd-commenter-20130908.2351/evil-nerd-commenter-pkg.el
+++
@@ -1,2 +0,0 @@
-(define-package "evil-nerd-commenter" "20130908.2351" "Comment/uncomment lines efficiently. Like Nerd Commenter in Vim [github]" 'nil)
-
--- dotfiles/emacs.d/elpa/tangotango-theme-20130827.1221/tangotango-theme-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; tangotango-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "tangotango-theme" "tangotango-theme.el" (21087
-;;;;;;  46834 31158 146000))
-;;; Generated autoloads from tangotango-theme.el
-
-(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("tangotango-theme-pkg.el") (21087 46834
-;;;;;;  101244 150000))
-
-;;;***
-
-(provide 'tangotango-theme-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; tangotango-theme-autoloads.el ends here
